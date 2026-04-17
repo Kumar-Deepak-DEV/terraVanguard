8d3aba9300f599ac1d92764ae92a437f5c5f62b0
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-04-17 14:46:38 +0530

    connection to database established and used all dependencies

---
+++ server/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
+
+// Connect to Database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:5174'], credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+
+
+
+
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: err.message });
+});
+
+
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
