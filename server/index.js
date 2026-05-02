0c6c69e409820d5e264652004ab2d15e8b73633f
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-05-02 10:29:31 +0530

    added web socket for real time chat sync

--- server/index.js
+++ server/index.js
@@ -15,21 +15,52 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
+// Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/ideas', require('./routes/ideaRoutes'));
 app.use('/api/events', require('./routes/eventRoutes'));
 app.use('/api/hall-of-fame', require('./routes/hallOfFameRoutes'));
+app.use('/api/notifications', require('./routes/notificationRoutes'));
 
-
-
+// Basic Error Handler
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: err.message });
 });
 
+const http = require('http');
+const { Server } = require('socket.io');
 
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: ['http://localhost:5173', 'http://localhost:5174'],
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
+
+// Make io accessible to our router
+app.set('io', io);
+
+io.on('connection', (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+
+    socket.on('join_idea_room', (ideaId) => {
+        socket.join(ideaId);
+        console.log(`User joined room: ${ideaId}`);
+    });
+
+    socket.on('leave_idea_room', (ideaId) => {
+        socket.leave(ideaId);
+        console.log(`User left room: ${ideaId}`);
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`User Disconnected: ${socket.id}`);
+    });
+});
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
