cec851e3505351178d356dd8103ba0d1ecc411d2
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-05-03 19:51:19 +0530

    updated cors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:5174'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:5174', 'https://team-terra.vercel.app'], credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -35,8 +35,8 @@ const { Server } = require('socket.io');
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173', 'http://localhost:5174'],
-        methods: ["GET", "POST"],
+        origin: ['http://localhost:5173', 'http://localhost:5174', 'https://team-terra.vercel.app'],
+        methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
     }
 });
